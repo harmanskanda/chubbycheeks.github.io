13ead496253cc55f092f6b96291e4a31bd7694a1
Harmanpal Singh <162496384+harmanskanda@users.noreply.github.com>
2024-08-04 02:29:40 +0530

    Some Changes

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@
 
     // Uncomment below and use your date //
 
-    /* var timerdate = "2020/12/30" */
+     var timerdate = "2024/08/31" 
 
     $("#countdown").countdown(timerdate, function (event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
